7514a881e5666fedf6cc68f2d4465eee4d0d13d9
christian renovell miralles <57290453+ChristianRenovell@users.noreply.github.com>
2023-09-10 12:44:23 +0100

    obtención de preguntas desde api

--- src/store/questionStorage.ts
+++ src/store/questionStorage.ts
@@ -1,6 +1,5 @@
-import { create, SetState } from "zustand";
-import { Question } from "../services/models/ResQuestions";
-
+import { create, SetState } from 'zustand';
+import { Question } from '../services/models/ResQuestions';
 
 interface Options {
   numQuestions: string;
@@ -18,7 +17,11 @@ interface State {
   setOptions: (newOptions: Options) => void;
   backQuestion: () => void;
   nextQuestion: () => void;
-  updateQuestion: (índice: number, nombrePropiedad: string, valorPropiedad: string | boolean)=> void;
+  updateQuestion: (
+    índice: number,
+    nombrePropiedad: string,
+    valorPropiedad: string | boolean
+  ) => void;
   getQuestions: () => void;
   getResultQuiz: () => number;
   resetState: () => number;
@@ -26,9 +29,9 @@ interface State {
 
 const useQuestionsStore = create<State>((set, get) => ({
   options: {
-    numQuestions: "",
-    difficulty: "",
-    category: "",
+    numQuestions: '',
+    difficulty: '',
+    category: '',
   },
   questions: [],
   currentQuestion: 0,
@@ -46,50 +49,80 @@ const useQuestionsStore = create<State>((set, get) => ({
   },
   nextQuestion: () => {
     set((state) => ({
-      currentQuestion: Math.min(state.currentQuestion + 1, state.totalQuestions - 1),
+      currentQuestion: Math.min(
+        state.currentQuestion + 1,
+        state.totalQuestions - 1
+      ),
     }));
   },
   getHists: () => {
     set((state) => {
-      return state
+      return state;
     });
   },
-  updateQuestion: (índice: number, nombrePropiedad: string, valorPropiedad: string | boolean) => {
+  updateQuestion: (
+    índice: number,
+    nombrePropiedad: string,
+    valorPropiedad: string | boolean
+  ) => {
     set((state) => {
       const preguntasActualizadas: Question[] = [...state.questions];
       if (índice >= 0 && índice < preguntasActualizadas.length) {
         preguntasActualizadas[índice][nombrePropiedad] = valorPropiedad;
       }
-      
-      state.quizCompleted = state.questions.findIndex((element) => element.selected_answer === null) === -1;
+
+      state.quizCompleted =
+        state.questions.findIndex(
+          (element) => element.selected_answer === null
+        ) === -1;
       return { questions: preguntasActualizadas };
     });
   },
   getQuestions: async () => {
     const options = useQuestionsStore.getState().options;
 
+    const body = {
+      model: 'gpt-3.5-turbo',
+      prompt: JSON.stringify(options),
+      max_tokens: 50,
+    };
+
+    const opciones = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    };
     try {
-      const response = await fetch('./src/services/mocks/resQuizz.json');
+      const response = await fetch(
+        'http://localhost:3000/quiz/completion',
+        opciones
+      );
       const resJson = await response.json();
+      const questionsJsonRes = JSON.parse(resJson[0].message.content);
+
       set({
-        totalQuestions: resJson.questions.length,
-        questions:resJson.questions,
-        loading: false
+        totalQuestions: questionsJsonRes.questions.length,
+        questions: questionsJsonRes.questions,
+        loading: false,
       });
     } catch (error) {
-      console.error("Error fetching questions:", error);
+      console.error('Error fetching questions:', error);
       return [];
     }
   },
   getResultQuiz: () => {
-    return get().questions.filter(element => element.correct_answer === element.selected_answer).length; 
+    return get().questions.filter(
+      (element) => element.correct_answer === element.selected_answer
+    ).length;
   },
   resetState: () => {
     set({
       options: {
-        numQuestions: "",
-        difficulty: "",
-        category: "",
+        numQuestions: '',
+        difficulty: '',
+        category: '',
       },
       questions: [],
       currentQuestion: 0,
